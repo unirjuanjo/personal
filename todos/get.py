cc81c70db25ab72aa0d00389c7f9af4886afd205
EC2 Default User <ec2-user@ip-172-31-93-3.ec2.internal>
2021-01-18 16:24:47 +0000

    Cambio en get.py

--- todos/get.py
+++ todos/get.py
@@ -4,7 +4,7 @@ import json
 from todos import decimalencoder
 import boto3
 dynamodb = boto3.resource('dynamodb')
-
+#Modificacion2
 
 def get(event, context):
     table = dynamodb.Table(os.environ['DYNAMODB_TABLE'])
